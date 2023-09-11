1a2aafb6e25cd86b0e80d436052f1daba5679cf6
michaelwaves <michaelyu713705@gmail.com>
2023-09-11 17:01:44 +0800

    passport apostrophe

--- components/popups/Passport.tsx
+++ components/popups/Passport.tsx
@@ -8,7 +8,7 @@ export default function Passport() {
                 Passport
             </h1>
             <p className="text-xl">
-                I'm a Canadian citizen, US permanent resident, and I hold a Hong Kong TTPS work visa.
+                I&#39;m a Canadian citizen, US permanent resident, and I hold a Hong Kong TTPS work visa.
             </p>
         </Popup>)
 
