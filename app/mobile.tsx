c6149276315b2889b663f4e24c8acbb91c84c336
michaelwaves <68243408+michaelwaves@users.noreply.github.com>
2023-09-13 19:22:35 +0800

    apostrophe

--- app/mobile.tsx
+++ app/mobile.tsx
@@ -34,7 +34,7 @@ export default function Mobile() {
             <div className='relative w-full h-full flex flex-col gap-4 md:gap-8'>
                 <div className="p-4">
                     <p className="md:text-2xl text-lg">
-                        Hi there. I&#39;m Michael, startup and tech enthusiast passionate about AI, Web3, and biotech. I'm currently interning
+                        Hi there. I&#39;m Michael, startup and tech enthusiast passionate about AI, Web3, and biotech. I&#39;m currently interning
                         at a clinical stage<UnderLine text="pharmaceutical startup" href="https://cloudbreakpharma.com/" />and building
                         a<UnderLine text="blockchain patent marketplace" href="https://trademint.org" />as part of the inaugural MLH Web3 Fellowship.
                         In my spare time I like to play badminton, cook tasty food, and build websites.
