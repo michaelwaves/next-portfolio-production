ddf222ee118caf5a12e9b7268e4508ef4cb22582
michaelwaves <68243408+michaelwaves@users.noreply.github.com>
2023-09-14 22:40:08 +0800

    mobile width

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { MOBILE_WIDTH } from '@/data/MobileWidth';
 import { useEffect, useState } from 'react';
 export default function Page() {
 
-  const [width, setWidth] = useState<number>(1000);
+  const [width, setWidth] = useState<number>(MOBILE_WIDTH);
   useEffect(() => {
     function handleWindowSizeChange() {
       setWidth(window.innerWidth);
