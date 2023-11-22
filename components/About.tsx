a505a503ec70a058db3a24ac662a346d16eabc49
michaelwaves <68243408+michaelwaves@users.noreply.github.com>
2023-11-22 20:00:03 +0800

    space

--- components/About.tsx
+++ components/About.tsx
@@ -2,10 +2,10 @@ import UnderLine from "./UnderLine"
 export default function About() {
     return (
         <p className="md:text-2xl text-lg">
-            Hi there. I&#39;m Michael, startup and tech enthusiast passionate about AI, Web3, and biotech. I&#39;m currently interning
+            Hi there. I&#39;m Michael, business and tech enthusiast passionate about Space, AI, Web3, and Biotech. I&#39;m currently interning
             at a clinical stage<UnderLine text="pharmaceutical startup" href="https://cloudbreakpharma.com/" />and building
             a<UnderLine text="blockchain patent marketplace" href="https://trademint.org" />as part of the inaugural MLH Web3 Fellowship.
-            In my spare time I like to play badminton, cook tasty food, and build websites.
+            In my spare time I like to play badminton and piano, cook tasty food, and build websites.
             Welcome to my corner of the internet :&#41;
         </p>
     )
--- components/popups/Blanket.tsx
+++ components/popups/Blanket.tsx
@@ -8,7 +8,8 @@ export default function Blanket() {
                 Blanket
             </h1>
             <p className="text-xl">
-                When I was in Air Cadets &#40;which I hated&#41; some goody two shoes cadet gave a big speech about how cadets gave him the discipline to make his bed everyday. I decided I didn&#39;t need to be a child soldier to have basic life skills.
+                I have a compulsive need to make my bed every morning.
+                {/* When I was in Air Cadets &#40;which I hated&#41; some goody two shoes cadet gave a big speech about how cadets gave him the discipline to make his bed everyday. I decided I didn&#39;t need to be a child soldier to have basic life skills. */}
             </p>
         </Popup>)
 
